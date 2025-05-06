4f8c28642f39bdabd86d059508cf361cc3858b65
iMprov <improv00@mail.ru>
2025-05-06 10:44:35 +0700

    полная инициализация БД. создание таблицы Users

--- app.rb
+++ app.rb
@@ -7,3 +7,29 @@ def init_db
 	@db = SQLite3::Database.new 'blog.db'
 	@db.results_as_hash = true
 end
+
+before do 
+
+	init_db
+
+end
+
+configure do 
+
+	init_db
+
+	@db.execute 'CREATE TABLE IF NOT EXISTS Users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  username TEXT,
+  password_hash TEXT,
+  is_admin BOOLEAN DEFAULT 0,
+  created_at DATE
+)'
+
+end
+
+get '/' do 
+
+	erb "Hello!"
+
+end
